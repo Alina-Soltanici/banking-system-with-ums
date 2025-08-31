0b40d6a5bdde02e3e36b4e7084688a5fc495e3af
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:21:54 +0300

    style(app): remove unused import and adjust formatting in FibankApplication

--- src/main/java/com/sistem_bank/fibank/FibankApplication.java
+++ src/main/java/com/sistem_bank/fibank/FibankApplication.java
@@ -2,13 +2,10 @@ package com.sistem_bank.fibank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-
-
 public class FibankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FibankApplication.class, args);
