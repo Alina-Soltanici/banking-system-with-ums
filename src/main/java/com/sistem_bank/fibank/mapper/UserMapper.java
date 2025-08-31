85a545ed08f80dc2a3c220cdb49122e5c21f7fa9
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:43:18 +0300

    feat:add UserMapper interface to convert from DTO->Entity and vice versa

--- src/main/java/com/sistem_bank/fibank/mapper/UserMapper.java
+++ src/main/java/com/sistem_bank/fibank/mapper/UserMapper.java
@@ -1,4 +1,18 @@
 package com.sistem_bank.fibank.mapper;
 
+import com.sistem_bank.fibank.dto.SignupRequest;
+import com.sistem_bank.fibank.dto.SignupResponse;
+import com.sistem_bank.fibank.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    User toEntity(SignupRequest signupRequest);
+
+   @Mapping (target = "message", constant = "registration completed successfully")
+    SignupResponse toSignupResponse(User user);
+
+
 }
