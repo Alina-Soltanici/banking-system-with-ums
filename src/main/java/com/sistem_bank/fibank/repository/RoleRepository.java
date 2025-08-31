6617b2dbb08595b62861ca9c42e941306a07105b
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:49:23 +0300

    feat:add findByName method in RoleRepository

--- src/main/java/com/sistem_bank/fibank/repository/RoleRepository.java
+++ src/main/java/com/sistem_bank/fibank/repository/RoleRepository.java
@@ -3,5 +3,8 @@ package com.sistem_bank.fibank.repository;
 import com.sistem_bank.fibank.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
 }
