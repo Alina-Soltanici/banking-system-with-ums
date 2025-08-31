9001e574163a1b9a1a49f33db440b2fe85c7ddb2
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:54:45 +0300

    feat(user):implement UserServiceImpl with signup functionality

--- src/main/java/com/sistem_bank/fibank/service/UserServiceImpl.java
+++ src/main/java/com/sistem_bank/fibank/service/UserServiceImpl.java
@@ -4,11 +4,38 @@ import com.sistem_bank.fibank.dto.LoginRequest;
 import com.sistem_bank.fibank.dto.LoginResponse;
 import com.sistem_bank.fibank.dto.SignupRequest;
 import com.sistem_bank.fibank.dto.SignupResponse;
+import com.sistem_bank.fibank.exceptions.RoleNotFoundException;
+import com.sistem_bank.fibank.mapper.UserMapper;
+import com.sistem_bank.fibank.model.Role;
+import com.sistem_bank.fibank.model.User;
+import com.sistem_bank.fibank.repository.RoleRepository;
+import com.sistem_bank.fibank.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
+
+
     @Override
     public SignupResponse signUp(SignupRequest signupRequest) {
-        return null;
+        User user = userMapper.toEntity (signupRequest);
+        user.setPassword (passwordEncoder.encode (signupRequest.getPassword ()));
+
+       Role defaultRole = roleRepository.findByName ("USER")
+               .orElseThrow (() -> new RoleNotFoundException ("Role not found"));
+        user.setRoles (Set.of (defaultRole));
+        User savedUser = userRepository.save (user);
+        return userMapper.toSignupResponse (savedUser);
     }
 
     @Override
