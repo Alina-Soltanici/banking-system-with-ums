150f25d6f421243483c970d66ccc640088cce842
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:02:13 +0300

    feat(config):add DataInitializer to insert default USER and ADMIN roles at startup

--- src/main/java/com/sistem_bank/fibank/config/DataInitializer.java
+++ src/main/java/com/sistem_bank/fibank/config/DataInitializer.java
@@ -1,4 +1,25 @@
 package com.sistem_bank.fibank.config;
 
-public class DataInitializer {
+import com.sistem_bank.fibank.model.Role;
+import com.sistem_bank.fibank.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+    private final RoleRepository roleRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(roleRepository.findByName ("USER").isEmpty ()) {
+            roleRepository.save (new Role (null, "USER"));
+        }
+
+        if(roleRepository.findByName ("ADMIN").isEmpty ()) {
+            roleRepository.save (new Role (null, "ADMIN"));
+        }
+    }
 }
