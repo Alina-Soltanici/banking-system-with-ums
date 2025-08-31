8eac0491eee092271e7691ccae8d82894d6378f3
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:52:08 +0300

    style: add blank line for readability in UserService

--- src/main/java/com/sistem_bank/fibank/service/UserService.java
+++ src/main/java/com/sistem_bank/fibank/service/UserService.java
@@ -6,6 +6,7 @@ import com.sistem_bank.fibank.dto.LoginResponse;
 import com.sistem_bank.fibank.dto.SignupRequest;
 import com.sistem_bank.fibank.dto.SignupResponse;
 
+
 public interface UserService {
     SignupResponse signUp(SignupRequest signupRequest);
     LoginResponse login(LoginRequest loginRequest);
