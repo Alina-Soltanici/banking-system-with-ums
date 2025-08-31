19ecb15fa497d33a6a0000855781ed2c771ef9aa
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:15:52 +0300

    feat(security):configure SecurityFilterChain with public signup/login endpoints & disable csrf

--- src/main/java/com/sistem_bank/fibank/config/SecurityConfig.java
+++ src/main/java/com/sistem_bank/fibank/config/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.sistem_bank.fibank.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf (csrf -> csrf.disable ())
+                .authorizeHttpRequests (request-> request.requestMatchers ("/api/user/signup", "/api/user/login").permitAll ().anyRequest ().authenticated ())
+                .build ();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder ();
+    }
 }
