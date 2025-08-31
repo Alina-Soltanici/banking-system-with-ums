4dbc7a0e7c39dd9d45599b22ec828f2ff31c60dc
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:30:03 +0300

    feat(user): add UserController with signup endpoint

--- src/main/java/com/sistem_bank/fibank/controller/UserController.java
+++ src/main/java/com/sistem_bank/fibank/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.sistem_bank.fibank.controller;
 
+import com.sistem_bank.fibank.dto.SignupRequest;
+import com.sistem_bank.fibank.dto.SignupResponse;
+import com.sistem_bank.fibank.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@AllArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest signupRequest) {
+        return new ResponseEntity<> (userService.signUp (signupRequest), HttpStatus.CREATED);
+    }
+
+//    @PostMapping
+//    public ResponseEntity
 }
