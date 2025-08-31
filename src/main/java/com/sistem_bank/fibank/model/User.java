b103c9dd48c56b3bc41814a387d8c2284e20404f
alinalinux <soltanicialina777@gmail.com>
2025-08-31 21:48:11 +0300

    feat:add createdAt field in User Entity

--- src/main/java/com/sistem_bank/fibank/model/User.java
+++ src/main/java/com/sistem_bank/fibank/model/User.java
@@ -3,16 +3,25 @@ package com.sistem_bank.fibank.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
 import java.util.Set;
 
 @Entity
 @Table(name = "users", schema = "auth")
 @Setter
 @Getter
+
+//NOTE: @EntityListeners(AuditingEntityListener.class) enable automatic populating
+// of the @CreatedDate and @LastModifiedDate fields when the entity is created or modified
+@EntityListeners (AuditingEntityListener.class)
 public class User {
-    @Id()
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false, updatable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+    @SequenceGenerator(name = "users_seq", sequenceName = "users_seq", allocationSize = 1)
     private Long id;
 
     @Column(name = "username", nullable = false, unique = true)
@@ -24,6 +33,13 @@ public class User {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Instant updatedAt;
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
