53246da2ce7866a278015731a9f97adc75b7dd34
alinalinux <soltanicialina777@gmail.com>
2025-09-01 08:34:57 +0300

    feat(api): change base path from /api/user to /api/auth

--- src/main/java/com/sistem_bank/fibank/controller/UserController.java
+++ src/main/java/com/sistem_bank/fibank/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/auth")
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
